3267551cbf38ef850c562fb7da17ea0ba5d032d2
Rob Sami <rob@benchling.com>
2016-07-10 23:09:16 -0700

    wip

--- app.js
+++ app.js
@@ -195,106 +195,33 @@ function receivedMessage(event) {
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
 
-  var isEcho = message.is_echo;
-  var messageId = message.mid;
-  var appId = message.app_id;
-  var metadata = message.metadata;
-
-  // You may get a text or attachment but not both
   var messageText = message.text;
   var messageAttachments = message.attachments;
-  var quickReply = message.quick_reply;
 
-  if (isEcho) {
-    // Just logging message echoes to console
-    console.log("Received echo for message %s and app %d with metadata %s", 
-      messageId, appId, metadata);
-    return;
-  } else if (quickReply) {
-    var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
-      messageId, quickReplyPayload);
-
-    sendTextMessage(senderID, "Quick reply tapped");
-    return;
+  if (messageText === 'image') {
+    request({
+      uri: 'https://hooks.slack.com/services/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',
+      method: 'POST',
+      json: {
+        text: 'user: ' + senderID + ' is trynna get in tho',
+        attachments: [
+          {
+            image_url: messageAttachments[0].payload.url,
+          },
+          {
+            text: 'let em in? http://www.robertsami.com'
+          },
+          {
+            text: 'fuck em? http://www.robertsami.com'
+          },
+        ],
+      },
+    }, function (error, response, body) {
+      console.log('yo');
+    });
+  } else {
+    sendTextMessage(senderID, 'Hi! Send us an image.');
   }
-
-  sendTextMessage(senderID, 'Hi! Send us an image.');
-  request({
-    uri: 'https://hooks.slack.com/services/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',
-    method: 'POST',
-    json: {
-      text: 'user: ' + senderID + ' is trynna get in tho',
-      attachments: [
-        {
-          image_url: messageAttachments[0].payload.url,
-        },
-      ],
-    },
-  }, function (error, response, body) {
-    console.log('yo');
-  });
-
-  // if (messageText) {
-
-  //   // If we receive a text message, check to see if it matches any special
-  //   // keywords and send back the corresponding example. Otherwise, just echo
-  //   // the text we received.
-  //   switch (messageText) {
-  //     case 'image':
-  //       sendImageMessage(senderID);
-  //       break;
-
-  //     case 'gif':
-  //       sendGifMessage(senderID);
-  //       break;
-
-  //     case 'audio':
-  //       sendAudioMessage(senderID);
-  //       break;
-
-  //     case 'video':
-  //       sendVideoMessage(senderID);
-  //       break;
-
-  //     case 'file':
-  //       sendFileMessage(senderID);
-  //       break;
-
-  //     case 'button':
-  //       sendButtonMessage(senderID);
-  //       break;
-
-  //     case 'generic':
-  //       sendGenericMessage(senderID);
-  //       break;
-
-  //     case 'receipt':
-  //       sendReceiptMessage(senderID);
-  //       break;
-
-  //     case 'quick reply':
-  //       sendQuickReply(senderID);
-  //       break        
-
-  //     case 'read receipt':
-  //       sendReadReceipt(senderID);
-  //       break        
-
-  //     case 'typing on':
-  //       sendTypingOn(senderID);
-  //       break        
-
-  //     case 'typing off':
-  //       sendTypingOff(senderID);
-  //       break        
-
-  //     default:
-  //       sendTextMessage(senderID, messageText);
-  //   }
-  // } else if (messageAttachments) {
-  //   sendTextMessage(senderID, "Message with attachment received");
-  // }
 }
 
 
